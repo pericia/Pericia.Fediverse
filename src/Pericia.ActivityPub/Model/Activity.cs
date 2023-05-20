9475def8987117da3d75d4c9ef56d9f24f10304d
Guillaume Lacasa <git@adhess.net>
2023-05-20 21:21:47 +0200

    Adding required properties to Activity

--- src/Pericia.ActivityPub/Model/Activity.cs
+++ src/Pericia.ActivityPub/Model/Activity.cs
@@ -12,19 +12,25 @@ public class Activity
 
     [JsonPropertyName("id")]
     public required string Id { get; set; }
-    
+
     [JsonPropertyName("type")]
     public string Type => "Note";
 
     [JsonPropertyName("actor")]
     public required string Actor { get; set; }
 
+    [JsonPropertyName("attributedTo")]
+    public required string AttributedTo { get; set; }
+
     [JsonPropertyName("content")]
     public required string Content { get; set; }
 
     [JsonPropertyName("url")]
     public required string Url { get; set; }
-    
+
     [JsonPropertyName("published")]
     public required DateTime Published { get; set; }
+
+    [JsonPropertyName("to")]
+    public string To => "https://www.w3.org/ns/activitystreams#Public";
 }
